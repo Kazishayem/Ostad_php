06fc60436619af430f822f1b0454c306ecb87a79
kazi <kazishayem1234@gmail.com>
2024-05-28 20:24:05 +0600

    json to text to array to json

--- try6.php
+++ try6.php
@@ -2,10 +2,28 @@
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
 
-    $plainTestJSON = file_get_contents("php://input");
-    echo $plainTestJSON;
+    $plainTestJSON = file_get_contents("php://input");  //json to text
+    // echo $plainTestJSON;
+
+    $plainTextToArray = json_decode($plainTestJSON , true); //text to array
+    // echo $plainTextToArray["num1"]+$plainTextToArray["num2"];
+
+    $sum = $plainTextToArray["num1"]+$plainTextToArray["num2"];
+    $sub = $plainTextToArray["num1"]-$plainTextToArray["num2"];
+    $mul = $plainTextToArray["num1"]*$plainTextToArray["num2"];
+    $div = $plainTextToArray["num1"]/$plainTextToArray["num2"];
+
+    $result = [
+        'sim' => $sum,
+        'sub' => $sub,
+        'mul' => $mul,
+        'div' => $div
+    ];
+
+    echo json_encode($result); //array to json
 
 }
+
 
 // if($_SERVER['REQUEST_METHOD'] == "GET"){
 
