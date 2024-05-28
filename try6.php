8250acc7abb0e65bee7ab11c7093eb0333d81606
kazi <kazishayem1234@gmail.com>
2024-05-28 20:27:44 +0600

    sim to sum

--- try6.php
+++ try6.php
@@ -14,7 +14,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     $div = $plainTextToArray["num1"]/$plainTextToArray["num2"];
 
     $result = [
-        'sim' => $sum,
+        'sum' => $sum,
         'sub' => $sub,
         'mul' => $mul,
         'div' => $div
